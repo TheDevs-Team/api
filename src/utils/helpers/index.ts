73f09b76690b11c37baccb94b839b251ccc0e317
gustavo-evaristo <gug.henri1@gmail.com>
2021-08-27 18:08:13 -0300

    feat:  decript password

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,12 +1,20 @@
-import { hashSync } from 'bcryptjs';
+import { compareSync, hashSync } from 'bcryptjs';
 import { CNPJ, CPF } from '~/utils';
 
-export const isValidPassword = (password: string, confirm_password: string): boolean => {
+export const isValidPassword = (password?: string, confirm_password?: string): boolean => {
   return password === confirm_password;
 };
 
 export const encryptPassword = (password: string): string => {
   return hashSync(password, 8);
+};
+
+export const decryptPassword = (password: string, passwordCompare: string): boolean => {
+  if (compareSync(password, passwordCompare)) {
+    return true;
+  }
+
+  return false;
 };
 
 export const isValidDocument = (document: string): boolean => {
