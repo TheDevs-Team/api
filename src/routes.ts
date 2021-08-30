22a790b13baee992e390e2c9855f4885293cdcb2
gustavo-evaristo <gug.henri1@gmail.com>
2021-08-29 21:10:58 -0300

    feat: invalid endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import UserController from './controllers/User';
 import { authentication } from './middlewares';
 
@@ -6,12 +6,17 @@ const routes = Router();
 
 routes.get('/', (req, res) => res.status(200).json({ ok: true }));
 
-routes.post('/user', authentication, UserController.index);
+routes.get('/user/find', authentication, UserController.index);
+routes.post('/user/login', UserController.login);
 routes.post('/user/create', UserController.create);
-routes.put('/user/update', UserController.update);
-routes.put('/user/disable', UserController.disable);
-routes.delete('/user/delete', UserController.delete);
-routes.get('/user/list', UserController.list);
-routes.post('/login', UserController.login);
+routes.put('/user/update', authentication, UserController.update);
+routes.put('/user/disable', authentication, UserController.disable);
+routes.delete('/user/delete', authentication, UserController.delete);
+routes.get('/user/list', authentication, UserController.list);
+
+routes.get('*', (req: Request, res: Response) => res.status(400).json({ error: 'no route found' }));
+routes.post('*', (req: Request, res: Response) => res.status(400).json({ error: 'no route found' }));
+routes.put('*', (req: Request, res: Response) => res.status(400).json({ error: 'no route found' }));
+routes.delete('*', (req: Request, res: Response) => res.status(400).json({ error: 'no route found' }));
 
 export default routes;
