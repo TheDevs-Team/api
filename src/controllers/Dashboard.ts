fd84e3f6a30ee250b5d213e8c413657b0bbc4545
gustavo-evaristo <gug.henri1@gmail.com>
2021-10-22 17:53:17 -0300

    feat: dashboard

--- src/controllers/Dashboard.ts
+++ src/controllers/Dashboard.ts
@@ -12,9 +12,14 @@ class DashBoardController {
     const pendings = (await User.find({ where: { financial_status: 'WAITING_PAYMENT' } })).length;
 
     const courses = (await Course.find()).length;
+    const newCourses = await Course.find({
+      order: { name: 'ASC' },
+      take: 3,
+    });
+
     const materials = (await Material.find()).length;
 
-    return res.status(200).json({ users, courses, materials, pendings });
+    return res.status(200).json({ users, courses, materials, pendings, newCourses });
   }
 
   async allUsers(req: Request, res: Response): Promise<Response> {
@@ -47,6 +52,14 @@ class DashBoardController {
     const materials = await Material.find();
 
     return res.status(200).json({ materials: materials.length });
+  }
+
+  async newCourses(req: Request, res: Response): Promise<Response> {
+    const Course = getRepository(CourseModel);
+
+    const courses = await Course.find({ order: { name: 'ASC' } });
+
+    return res.status(200).json({ courses: courses.length });
   }
 }
 
