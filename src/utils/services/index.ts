8c585e219d615f3b56db3ecbbfe46a40c6967ad3
gustavo-evaristo <gug.henri1@gmail.com>
2021-12-14 18:51:04 -0300

    fix: email

--- src/utils/services/index.ts
+++ src/utils/services/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import nodemailer from 'nodemailer';
 
-export const sendMail = (to: string, subject: string, html: string): void => {
+export const sendMail = (to: string, name: string, password: string): void => {
   // Configurando conta para enviar e-mails
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -17,7 +17,7 @@ export const sendMail = (to: string, subject: string, html: string): void => {
   transporter.sendMail({
     from: 'G2K Investimentos <gug.henri2@gmail.com>',
     to,
-    subject,
-    html,
+    subject: 'Cadastro Realizado com sucesso',
+    html: `<h2>${name} boas noticias<h2> <br> <h3>Seu cadastro na plataforma G2K foi realizada com sucesso, segue os seus dados de acesso: <br> <p><strong>login: </strong> ${to} <br> <p><strong>Senha: </strong> ${password}</h3> `,
   });
 };
