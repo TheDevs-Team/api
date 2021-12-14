62b0c0ac7ee329c760656cbad0327719a51c87fe
gustavo-evaristo <gug.henri1@gmail.com>
2021-12-14 20:27:31 -0300

    fix: errors

--- src/controllers/Notification.ts
+++ src/controllers/Notification.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { sendMail } from '~/utils';
+import { sendMailPendings, sendMailClass, sendMailNewCourses } from '~/utils';
 import { User as UserModel } from '~/models';
 import { getRepository } from 'typeorm';
 
@@ -15,11 +15,7 @@ class NotificationController {
       if (pendings) {
         users.map((user: UserType) => {
           if (user.financial_status === 'WAITING_PAYMENT') {
-            return sendMail(
-              user.email,
-              'Pagamento',
-              `<strong>${user.name}</strong> Você possui pendencias com a G2K. <br>Entre em contato com a instituição e regulariza sua situação.<br><br>(11) 98765-4321  -  contato@g2kinvestimentos.com.br`,
-            );
+            return sendMailPendings(user.email);
           }
 
           return;
@@ -28,24 +24,12 @@ class NotificationController {
 
       if (new_courses) {
         setTimeout(() => {
-          users.map((user: UserType) =>
-            sendMail(
-              user.email,
-              'Novos cursos disponíveis',
-              `<strong>${user.name}</strong>, Novos cursos foram disponibilizados na plataforma, corra e evolua seus estudos para o proximo nível. <br> <a href="#">www.google.com</a>`,
-            ),
-          );
+          users.map((user: UserType) => sendMailNewCourses(user.email));
         }, 60000);
       }
 
       if (class_notification) {
-        users.map((user: UserType) =>
-          sendMail(
-            user.email,
-            'Lembrete de aula',
-            `<strong>${user.name}</strong>, Estamos aqui para lembrar e motivar os seus estudos, acesse a plataforma e de continuidade a seus estudos. <br> <a href="#">www.google.com</a>`,
-          ),
-        );
+        users.map((user: UserType) => sendMailClass(user.email));
       }
 
       return res.status(200).json({ ok: true });
